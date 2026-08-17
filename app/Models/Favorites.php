d8ad8726e7bad8e5b73700a1751c39283d3d379c
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-08-17 15:53:10 +0300

    feat: adding relations to favorites model

--- app/Models/Favorites.php
+++ app/Models/Favorites.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorites extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'receipt_id', 'user_id');
+    }
 }
